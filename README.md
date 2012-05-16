4115be01980e9a7aee5025f9b886b3db5bd58c64
Pelle Braendgaard <pelle@stakeventures.com>
2012-05-16 16:18:07 -0300

    bump to 0.0.5

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ We recommend using [clj-http](https://github.com/dakrone/clj-http) for performin
 
 Add the following to your project.clj's dependencies section:
 
-    [oauthentic "0.0.4"]
+    [oauthentic "0.0.5"]
 
 Import the library:
 
--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject oauthentic "0.0.4"
+(defproject oauthentic "0.0.5"
   :description "Simple OAuth2 client library"
   :dependencies  [[org.clojure/clojure "1.4.0"]
-                  [clj-http "0.4.1"]]
+                  [clj-http "0.4.1"]
+                  [ring/ring-core "1.1.0"]]
   :dev-dependencies 
                   [[clj-http-fake "0.3.0"]
                    [clauth "1.0.0-rc1"]])
