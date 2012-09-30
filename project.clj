21973966001f06bc43463dccb877e7a2d162c2c4
Pelle Braendgaard <pelle@stakeventures.com>
2012-09-30 17:11:41 -0400

    Update to use leiningen 2

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies  [[org.clojure/clojure "1.4.0"]
                   [clj-http "0.4.1"]
                   [ring/ring-core "1.1.0"]]
-  :dev-dependencies 
-                  [[clj-http-fake "0.3.0"]
-                   [clauth "1.0.0-rc4"]])
+  :profiles {
+    :dev {
+      :dependencies [[clj-http-fake "0.3.0"]
+                     [clauth "1.0.0-rc4"]]}})
