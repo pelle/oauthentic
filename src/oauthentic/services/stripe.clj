011d8ad13b4b3650424d2e11d3f1506e2629c1ff
Pelle Braendgaard <pelle@stakeventures.com>
2012-10-04 11:56:04 -0400

    Add refresh token support for stripe

--- src/oauthentic/services/stripe.clj
+++ src/oauthentic/services/stripe.clj
@@ -4,15 +4,12 @@
 ;; https://stripe.com/docs/apps/oauth
 (defmethod oauth/token-request :stripe
   [params]
-  { :accept :json :as :json
-    :form-params (-> params
-                    (dissoc :redirect-uri :client_id)
-                    (assoc :client_id (:client-id params)
-                            :redirect_uri (:redirect-uri params))
-                    (select-keys [:code :scope :redirect_uri])
-                    (assoc :client_id (:client-id params)
-                           :grant_type "authorization_code"))
-    :oauth-token (:client-secret params)})
+  (-> params
+      (dissoc :service)
+      (oauth/token-request)
+      (assoc-in [:form-params :client_id] (:client-id params))
+      (dissoc :basic-auth)
+      (assoc :oauth-token (:client-secret params))))
 
 (defmethod oauth/build-authorization-url :stripe [this params]
   (oauth/build-authorization-url "https://connect.stripe.com/oauth/authorize" params))
